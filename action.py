5e5765298afd391601d26b8fcb93df29f1e28449
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-20 04:56:02 -0700

    File to track all actions like player movement

---
+++ action.py
@@ -0,0 +1,11 @@
+class Action:
+    pass
+
+class Escape(Action):
+    pass
+
+class Movement(Action):
+    def __init__(self, dx: int, dy: int):
+        super().__init__()
+        self.dx = dx
+        self.dy = dy
