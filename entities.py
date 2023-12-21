7c8228b884c0ab09609e68d8965680b7e7667b36
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-20 18:56:12 -0700

    Renamed variable char to entity_char

--- entities.py
+++ entities.py
@@ -2,10 +2,10 @@ from typing import Tuple
 
 # Class for all entities including player, items, enemies, and others
 class Entity:
-    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+    def __init__(self, x: int, y: int, entity_char: str, color: Tuple[int, int, int]):
         self.x = x
         self.y = y
-        self.char = char
+        self.entity_char = entity_char
         self.color = color
     
     def move(self, dx: int, dy: int) -> None:
