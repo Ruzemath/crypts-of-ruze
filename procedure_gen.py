de4bfc0777bd807a798efb7234ed5689b06ce926
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 01:32:25 -0700

    Remade the generate_dungeon to create dungeons with random rooms and tunnel connections

--- procedure_gen.py
+++ procedure_gen.py
@@ -3,7 +3,9 @@ import random
 import tile_types
 import tcod
 from map import DungeonMap
-from typing import Iterator, Tuple
+from typing import Iterator, Tuple, List, TYPE_CHECKING
+if TYPE_CHECKING:
+    from entities import Entity
 
 class RectRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -42,14 +44,36 @@ def L_tunnel(begin: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tuple[int
         yield x, y
     
     
-def generate_dungeon(map_width, map_height) -> DungeonMap:
+def generate_dungeon(max_rooms: int, min_room_size: int, max_room_size: int, 
+                     map_width: int, map_height: int, player: Entity) -> DungeonMap:
+    
     dungeon = DungeonMap(map_width, map_height)
-    room1 = RectRoom(x = 20, y = 15, width = 10, height = 15)
-    room2 = RectRoom(x = 35, y = 15, width = 10, height = 15)
-    
-    dungeon.tiles[room1.inner] = tile_types.floor
-    dungeon.tiles[room2.inner] = tile_types.floor
-    
-    for x, y in L_tunnel(room2.center, room1.center):
-        dungeon.tiles[x,y] = tile_types.floor
+    rooms: List[RectRoom] = []
+
+    for r in range(max_rooms):
+        room_width = random.randint(min_room_size, max_room_size)
+        room_height = random.randint(min_room_size, max_room_size)
+
+        x = random.randint(0, dungeon.width - room_width - 1)
+        y = random.randint(0, dungeon.height - room_height - 1)
+
+        # RectRoom class makes rectangles easier to work with
+        new_room = RectRoom(x, y, room_width, room_height)
+
+        # Run through the other rooms and see if they intersect with this one.
+        if any(new_room.intersects(other_room) for other_room in rooms):
+            continue  # This room intersects, so go to the next attempt.
+        # If there are no intersections then the room is valid.
+
+        # Dig out this rooms inner area.
+        dungeon.tiles[new_room.inner] = tile_types.floor
+
+        if len(rooms) == 0: # player room
+            player.x, player.y = new_room.center
+        else:  
+            for x, y in L_tunnel(rooms[-1].center, new_room.center):
+                dungeon.tiles[x, y] = tile_types.floor
+
+        rooms.append(new_room)
+
     return dungeon
