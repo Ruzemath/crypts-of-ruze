39c44ea7b72f651eeb7cbfb69b4e925d9abd29b9
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 15:55:40 -0700

    Changed tiles_rgb to rgb

--- map.py
+++ map.py
@@ -15,7 +15,7 @@ class DungeonMap:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def make(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+        console.rgb[0:self.width, 0:self.height] = np.select(
             condlist = [self.visible, self.encountered],
             choicelist = [self.tiles["light"], self.tiles["dark"]],
             default = tile_types.VOID
