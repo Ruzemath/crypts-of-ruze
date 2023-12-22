877f5415aeb9e1886487ab7347222420a0bd32b3
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-21 20:30:16 -0700

    Utilized map class for generator to use to make the dungeon

--- generator.py
+++ generator.py
@@ -4,11 +4,13 @@ from tcod.console import Console
 from action import Leave, Movement
 from entities import Entity
 from input_handler import EventHandler
+from map import DungeonMap
 
 class Generator:
-    def __init__(self, entities: Set[Entity], event_handle: EventHandler, player: Entity):
+    def __init__(self, entities: Set[Entity], event_handle: EventHandler, dungeon_map: DungeonMap, player: Entity):
         self.entities = entities
         self.event_handle = event_handle
+        self.dungeon_map = dungeon_map
         self.player = player
     
     def handle(self, events: Iterable[Any]) -> None:
@@ -19,11 +21,13 @@ class Generator:
                 continue
             
             if isinstance(action, Movement):
-                    self.player.move(dx = action.dx, dy = action.dy)
+                if self.dungeon_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy]:
+                    self.player.move(action.dx, action.dy)
             elif isinstance(action, Leave):
                     raise SystemExit()
     
     def make(self, console: Console, context: Context) -> None:
+        self.dungeon_map.make(console)
         for entity in self.entities:
             console.print(entity.x, entity.y, entity.entity_char, fg = entity.color)
         
