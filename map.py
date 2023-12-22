21130a5eafe8823937bd1631bc88e1c69dce6396
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 15:46:51 -0700

    Incorporated FOV into map when creating it

--- map.py
+++ map.py
@@ -8,9 +8,15 @@ class DungeonMap:
         self.width = width
         self.height = height
         self.tiles = np.full((width, height), fill_value = tile_types.wall, order = "F")
+        self.visible = np.full((width, height), fill_value = False, order = "F")
+        self.encountered = np.full((width, height), fill_value = False, order = "F")
 
     def bounds_check(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def make(self, console: Console) -> None:
-        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist = [self.visible, self.encountered],
+            choicelist = [self.tiles["light"], self.tiles["dark"]],
+            default = tile_types.VOID
+        )
