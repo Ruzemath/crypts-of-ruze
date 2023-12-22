ab2966b24c037e5d001b7e6aeeb2ff78be977681
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-21 20:31:46 -0700

    Added to method act to Action class and its subclasses (Leave and Movement) that will be in charge of the specific actions that subclass will preform

--- action.py
+++ action.py
@@ -1,11 +1,36 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from generator import Generator
+    from entities import Entity
+
 class Action:
-    pass
+    def act(self, generator: Generator, entity: Entity) -> None:
+        raise NotImplementedError()
+        """Perform this action with the objects needed to determine its scope.
+        `generator` is the scope this action is being performed in.
+        `entity` is the object performing the action.
+        This method must be overridden by Action subclasses.
+        """
 
 class Leave(Action):
-    pass
+    def act(self, generator: Generator, entity: Entity) -> None:
+        raise SystemExit()
 
 class Movement(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
         self.dx = dx
         self.dy = dy
+    
+    def act(self, generator: Generator, entity: Entity) -> None:
+        new_x = entity.x + self.dx
+        new_y = entity.y + self.dy
+        
+        if not generator.dungeon_map.bounds_check(new_x, new_y): # Can't Move, Out of Bounds
+            return
+        if not generator.dungeon_map.tiles["walkable"][new_x, new_y]: # Can't Move, Tile is Not Walkable
+            return
+
+        entity.move(self.dx, self.dy)
