4e92a72e48b150c98d9edb0c7cb71b290451c562
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-21 20:47:04 -0700

    Updated main file with all the necessary changes made through the map, generator and action files. Player icon now will not be able to move out of bounds from dungeon map and non-walkable tiles

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import tcod
-
 from generator import Generator
 from input_handler import EventHandler
 from entities import Entity
+from map import DungeonMap
 
 
 def main() -> None:
-    screen_width = 150
-    screen_height = 100
+    screen_width = 80
+    screen_height = 50
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -16,9 +18,10 @@ def main() -> None:
 
     event_handle = EventHandler() 
     player = Entity(int(screen_width/2), int(screen_height/2), "#", (255, 255, 255))
-    npc = Entity(int((screen_width/2)-5), int(screen_height/2), "NPC", (0, 255, 0))
+    npc = Entity(int((screen_width/2)-5), int(screen_height/2), "N", (0, 255, 0))
     entities = {player, npc}
-    generator = Generator(entities, event_handle, player)
+    dungeon_map = DungeonMap(map_width, map_height)
+    generator = Generator(entities, event_handle, dungeon_map, player)
     
     # Console 
     with tcod.context.new_terminal(
