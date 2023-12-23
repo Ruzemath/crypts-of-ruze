fc496a85ce69fe2e188f95b04c2545e5ad8112ac
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 23:32:26 -0700

    Created a class called DirectionAction that be the new Superclass for Movement class and two other classes called Attack and ActionOfChoice, where ActionOfChoice will decide between Attack or Movement

--- action.py
+++ action.py
@@ -13,12 +13,29 @@ class Action:
 class Leave(Action):
     def act(self, generator: Generator, entity: Entity) -> None:
         raise SystemExit()
-
-class Movement(Action):
+    
+class DirectionAction(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
         self.dx = dx
         self.dy = dy
+
+    def act(self, generator: Generator, entity: Entity) -> None:
+        raise NotImplementedError()
+
+class Attack(DirectionAction):
+    def act(self, generator: Generator, entity: Entity) -> None:
+        new_x = entity.x + self.dx
+        new_y = entity.y + self.dy
+        target = generator.dungeon_map.get_blocking_entity_at_location(new_x, new_y)
+        
+        if not target: # No entity to attack.
+            return  
+
+        print(f"You kick the {target.name}, much to its annoyance!")
+
+
+class Movement(DirectionAction):
     
     def act(self, generator: Generator, entity: Entity) -> None:
         new_x = entity.x + self.dx
@@ -28,5 +45,17 @@ class Movement(Action):
             return
         if not generator.dungeon_map.tiles["walkable"][new_x, new_y]: # Can't Move, Tile is Not Walkable
             return
-
+        if generator.dungeon_map.get_blocking_entity_at_location(new_x, new_y): # Can't Move, Entity on Tile
+            return
+        
         entity.move(self.dx, self.dy)
+
+class ActionOfChoice(DirectionAction):
+    def act(self, generator: Generator, entity: Entity) -> None:
+        new_x = entity.x + self.dx
+        new_y = entity.y + self.dy
+
+        if generator.dungeon_map.get_blocking_entity_at_location(new_x, new_y):
+            return Attack(self.dx, self.dy).act(generator, entity)
+        else:
+            return Movement(self.dx, self.dy).act(generator, entity)
