76cc5fe959f9e71111755877ab044617b81d9a13
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 23:33:44 -0700

    Movement has been replaced by ActionOfChoice

--- input_handler.py
+++ input_handler.py
@@ -1,6 +1,6 @@
 from typing import Optional
 import tcod.event
-from action import Action, Leave, Movement
+from action import Action, ActionOfChoice, Leave
 
 
 class EventHandler(tcod.event.EventDispatch[Action]):
@@ -13,13 +13,13 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         key = event.sym
 
         if key == tcod.event.KeySym.UP or key == tcod.event.KeySym.w:
-            action = Movement(dx = 0, dy = -1)
+            action = ActionOfChoice(dx = 0, dy = -1)
         elif key == tcod.event.KeySym.DOWN or key == tcod.event.KeySym.s:
-            action = Movement(dx = 0, dy = 1)
+            action = ActionOfChoice(dx = 0, dy = 1)
         elif key == tcod.event.KeySym.LEFT or key == tcod.event.KeySym.a:
-            action = Movement(dx = -1, dy = 0)
+            action = ActionOfChoice(dx = -1, dy = 0)
         elif key == tcod.event.KeySym.RIGHT or key == tcod.event.KeySym.d:
-            action = Movement(dx = 1, dy = 0)
+            action = ActionOfChoice(dx = 1, dy = 0)
         elif key == tcod.event.KeySym.ESCAPE:
             action = Leave()
 
