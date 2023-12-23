e969c40c846162e8034e66031df7c062f860dd67
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 22:08:33 -0700

    Added .py to file

---
+++ entity_list.py
@@ -0,0 +1,5 @@
+from entities import Entity
+
+player = Entity(char = "@", color = (255, 255, 255), name = "Player", blocks_movement = True)
+goblin = Entity(char = "G", color = (51, 153, 51), name = "Goblin", blocks_movement = True)
+hobgoblin = Entity(char = "H", color = (255, 77, 77), name = "Hobgoblin", blocks_movement = True)
