13af22b68279b4f737d7fcdda87ddcdc3b04c9c6
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-22 22:19:33 -0700

    Added names and block movement access to entity constructor and make a spawn function that in charge of adding entities to map

--- entities.py
+++ entities.py
@@ -1,12 +1,29 @@
-from typing import Tuple
+from __future__ import annotations
+import copy
+from typing import Tuple, TypeVar, TYPE_CHECKING
+if TYPE_CHECKING:
+    from map import DungeonMap
 
+T = TypeVar("T", bound = "Entity")
 # Class for all entities including player, items, enemies, and others
 class Entity:
-    def __init__(self, x: int, y: int, entity_char: str, color: Tuple[int, int, int]):
+    def __init__(self,x: int = 0, y: int = 0, char: str = "?", 
+                 color: Tuple[int, int, int] = (255, 255, 255), 
+                 name: str = "<Unnamed>", blocks_movement: bool = False,):
+        
         self.x = x
         self.y = y
-        self.entity_char = entity_char
+        self.char = char
         self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+    
+    def spawn(self: T, dungeon_map: DungeonMap, x: int, y: int) -> T:
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        dungeon_map.entities.add(clone)
+        return clone
     
     def move(self, dx: int, dy: int) -> None:
         self.x += dx
