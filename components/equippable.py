bddfeefc64098c1cd0c07ec2ae4d525d1603cd9e
Ruzemath <82734181+Ruzemath@users.noreply.github.com>
2023-12-29 21:06:33 -0700

    Classes of equipment that provide certain boosts

---
+++ components/equippable.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from components.base_component import BaseComponent
+from equipment_types import EquipmentType
+if TYPE_CHECKING:
+    from entities import Item
+
+
+class Equippable(BaseComponent):
+    parent: Item
+
+    def __init__(
+        self,
+        equipment_type: EquipmentType,
+        power_bonus: int = 0,
+        defense_bonus: int = 0,
+        health_bonus: int = 0,
+    ):
+        self.equipment_type = equipment_type
+        self.power_bonus = power_bonus
+        self.defense_bonus = defense_bonus
+        self.health_bonus = health_bonus
+
+
+class BronzeDagger(Equippable):
+    def __init__(self) -> None:
+        super().__init__(equipment_type = EquipmentType.WEAPON, power_bonus = 2)
+
+class SteelSword(Equippable):
+    def __init__(self) -> None:
+        super().__init__(equipment_type = EquipmentType.WEAPON, power_bonus = 6)
+
+class LeatherTunic(Equippable):
+    def __init__(self) -> None:
+        super().__init__(equipment_type = EquipmentType.ARMOR, defense_bonus = 1)
+
+class SteelPlate(Equippable):
+    def __init__(self) -> None:
+        super().__init__(equipment_type = EquipmentType.ARMOR, defense_bonus = 3)
+
+class BronzeRing(Equippable):
+    def __init__(self) -> None:
+        super().__init__(equipment_type = EquipmentType.RING, health_bonus = 25)
+
+class SteelRing(Equippable):
+    def __init__(self) -> None:
+        super().__init__(equipment_type = EquipmentType.RING, health_bonus = 75)
+
